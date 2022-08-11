e03a53e790354b67cd380d9dcb43d958c6f2e749
KINghyuk9 <dlgur9612@gmail.com>
2022-08-11 16:27:49 +0900

    B1330 1/1 B9498 1/1 B10809 재도전

---
+++ src/backjoon/B10809.java
@@ -0,0 +1,21 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class B10809 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String alpha = sc.next();
+
+        int arr[] = new int[26];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = -1;
+        }
+        for(int j=0; j< alpha.length(); j++){
+            char c = alpha.charAt(j);
+            int tmp = (int)c;
+
+        }
+    }
+
+}
