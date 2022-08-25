a6f294dccd1104d9ee3fdd1798936ccfbee55b84
KINghyuk9 <dlgur9612@gmail.com>
2022-08-25 16:57:57 +0900

    B100951 B100952 B1110 <- 다시풀것 해결못함.

---
+++ src/backjoon/B1110.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class B1110 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int count = 0;
+        int before = N;
+
+        while (true){
+
+        }
+    }
+}
